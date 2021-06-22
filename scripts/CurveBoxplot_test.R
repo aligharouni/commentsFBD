d4c0b387147fc7d8f94487fe6d8ad7f4e99a5204
aligharouni <aligharouni@gmail.com>
2021-06-22 11:59:08 -0400

    working

--- scripts/CurveBoxplot_test.R
+++ scripts/CurveBoxplot_test.R
@@ -2,6 +2,7 @@
 # 1- Juul's algorithm with J=2 and fda match?
 # 2- FBD + J=50 in the comparison
 # 3- do roahd and fda give similar curves?
+
 source('functions.R')
 library(tidyverse)
 library(latex2exp)
@@ -31,9 +32,6 @@ EnRkTemp_J <- EnsRank_all(ensemble_J,numSample=100,sizeSample=50) ##  as Juul's 
 rnkmat <- EnRkTemp_J[["ensembRank"]] ## take the resulted rank mat
 
 md1 <- rank(colSums(rnkmat)) ## , ties="random")
-plot(sort(md1)) ## Note the discontinuity!?
-## Discontinuity is due to ties; if ties were broken with 'random' this
-## would go away
 central_curves <- which(md1>quantile(md1,0.5))
 
 envelope_list <- c(envelope_list,
@@ -57,7 +55,7 @@ envelope_list <- c(envelope_list,
 
 fD <- fData(tvec,as.matrix(t(ensemble_J))) ## note that the trajs are on rows in fD object
 # dev.new()
-ff <- fbplot(fD,method='BD', plot=FALSE, #method='MBD'
+ff <- roahd::fbplot(fD,method='MBD', plot=FALSE, #method='MBD'
              outliercol = 2,
              fullout = FALSE,
              outline=FALSE,
@@ -68,21 +66,23 @@ ff <- fbplot(fD,method='BD', plot=FALSE, #method='MBD'
 ## if plot=false, it outputs the calculations and scores
 
 envelope_list <- c(envelope_list,
-                   list(fda=get_envelope(ensemble_J, ff$Depth>median(ff$Depth))))
+                   list(fda_roahd=get_envelope(ensemble_J, ff$Depth>median(ff$Depth))))
+###################################
+## Settings/plotting
+###################################
 envdat <- dplyr::bind_rows(envelope_list, .id="method")
 
 long_ensemble <- ensemble_J %>% as.matrix() %>% reshape2::melt() %>%
   rename(tvec="Var1",grp="Var2") %>% mutate(across(tvec, ~.-1))
 
 theme_set(theme_bw())
-## ggplot(envdat, aes(tvec)) + geom_ribbon(aes(ymin=lwr,ymax=upr),colour=NA, alpha=0.4, fill="blue") +
-##    facet_wrap(~method) + geom_line(data=long_ensemble, aes(y=value,group=grp), alpha=0.05)
 
 labvec <- c(juul = "FBD\n($J = 50$)",
-            fda = "FBD\n($J = 2$)",
-            mahal = "Mahalanobis\n(6 features)",
+            fda_roahd = "FBD\n($J = 2$)",
             juul_J2 = "Juul\n($J = 2$)"
 )
+
+envdat <- mutate(envdat, across(method, ~ labvec[.]))
 
 envdat_temp <- envdat %>% filter(method==c("FBD\n($J = 2$)","Juul\n($J = 2$)"))
 cent_plot2 <- (ggplot(envdat_temp, aes(tvec))
@@ -90,5 +90,5 @@ cent_plot2 <- (ggplot(envdat_temp, aes(tvec))
 )
 print(cent_plot2)
 
-
+# rm(list=ls())
 
